a406d34ea4ec3795dd9c608fc334bab7171d8f37
nishant patel <nishantpatelbca@gmail.comgit config --global user.name nishantpatel11>
2019-10-20 12:45:47 +0530

    Added Thread Executor Project

---
+++ thread-executor-services/src/main/java/com/thread/executor/services/App.java
@@ -0,0 +1,13 @@
+package com.thread.executor.services;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
